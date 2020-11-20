865a58c0b38c06c7ed0cd72f22530fab93707c82
Mayistikar <andersonrodriguezce@gmail.com>
2020-11-19 22:15:19 -0500

    Adding bootstrap and testing with a navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
-const App = () => (<h1>Administrador de pacientes</h1>);
+import healthLogo from './assets/health-report.svg';
+
+const App = () => (
+  <nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">
+      <img src={healthLogo} width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy"/>
+      Administrador de pacientes
+    </a>
+  </nav>
+);
 
 export default App;
